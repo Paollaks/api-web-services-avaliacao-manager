6307b5b13e19e61a0e3e677c5d529f32723ca4b0
Paollaks <paollaks@hotmail.com>
2025-03-16 16:04:45 -0300

    Configuração do EntifyFramework

---
+++ api-web-services-avaliacao-manager/Models/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace api_web_services_avaliacao_manager.Models
+{
+    public class AppDbContext : DbContext 
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {         
+        }
+
+        public DbSet<Filme> Filmes { get; set; }
+        public DbSet<Comentario> Comentarios { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
--- api-web-services-avaliacao-manager/Program.cs
+++ api-web-services-avaliacao-manager/Program.cs
@@ -1,8 +1,13 @@
+using api_web_services_avaliacao_manager.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
