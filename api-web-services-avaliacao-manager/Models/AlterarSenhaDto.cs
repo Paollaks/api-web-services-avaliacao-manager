905f2df53bb66223c87f5688106b283790e14377
Carlos Santos <108103210+CarlosSanctorum@users.noreply.github.com>
2025-05-21 04:47:33 -0300

    adicionado a funcionalidade de alterar senh do usuario.

---
+++ api-web-services-avaliacao-manager/Models/AlterarSenhaDto.cs
@@ -0,0 +1,8 @@
+﻿namespace api_web_services_avaliacao_manager.Models
+{
+    public class AlterarSenhaDto
+    {
+        public string SenhaAtual { get; set; }
+        public string NovaSenha { get; set; }
+    }
+}
